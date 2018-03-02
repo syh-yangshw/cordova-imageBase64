9b4dfa9f947dede2b4b5db7b2e44169ed433ac4a
yangshiwei <yangshiwei@syhvip.com>
2018-03-02 14:37:22 +0800

    解决多张图片转码出现重复的问题

--- www/ImageBase64.js
+++ www/ImageBase64.js
@@ -3,10 +3,10 @@ var ImageBase64 = function() {
 
 ImageBase64.prototype.base64FromUrl = function(url, success, fail) {
   var fileTransfer = new FileTransfer();
-
+  var dateTime=new Date().getTime();
   fileTransfer.download(
     url,
-    cordova.file.cacheDirectory + '/imageBase64Cache.jpg',
+    cordova.file.cacheDirectory + '/imageBase64Cache_'+dateTime+'.jpg',
     function(entry) {
       var imagePath = entry.toURL().replace(/^file:\/\//i, '');
       return cordova.exec(success, fail, 'ImageBase64', 'base64', [imagePath]);
